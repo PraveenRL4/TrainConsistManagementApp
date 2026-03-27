be95471b8dddb3555ae0e2d3f8c03480241a6151
PraveenRL4 <PraveenRL4@users.noreply.github.com>
2026-03-27 14:30:36 +0530

    UC1: Initialize Train and Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -5,12 +5,13 @@ public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
+        // Welcome Message
         System.out.println("=== Train Consist Management App ===");
 
+        // Initialize Train Consist
         List<String> trainConsist = new ArrayList<>();
 
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + trainConsist.size());
-
     }
 }
